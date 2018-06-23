4ae4e7fe580973637974d8a0ad966e11c1aac7e4
Vojtiik <vojtech.mares@justgiving.com>
2018-06-23 22:05:22 +0100

    take 1, 1 fail left

---
+++ src/Chopper.cs
@@ -0,0 +1,75 @@
+using System;
+using take_1;
+
+public class BinaryChop : IChopper
+{
+    private int ChopSlice(int numberToFind, int[] array_of_int, int indexStart, int indexto)
+    {
+        var segment = new ArraySegment<int>(array_of_int, indexStart, indexto).ToArray();
+
+        int length = segment.Length;
+        var middleIndex = length / 2;
+        int valueInTheMiddle = segment[middleIndex];
+        if (valueInTheMiddle == numberToFind)
+        {
+            Console.WriteLine("found: " + numberToFind);
+            return indexStart + middleIndex;
+        }
+
+        if (length == 1)
+        {
+            Console.WriteLine("not found: " + numberToFind);
+            return -1;
+        }
+
+        if (numberToFind < valueInTheMiddle)
+        {
+            var index = ChopSlice(numberToFind, array_of_int, indexStart, middleIndex);
+            return index;
+        }
+        else
+        {
+            var index = ChopSlice(numberToFind, array_of_int, indexStart + middleIndex, middleIndex);
+            return index;
+        }
+    }
+
+    public int Chop(int numberToFind, int[] array_of_int)
+    {
+
+        Console.WriteLine("looking for " + numberToFind);
+        int length = array_of_int.Length;
+
+        if (length == 0)
+        {
+            return -1;
+        }
+
+        if (length == 1)
+        {
+            if (array_of_int[0] == numberToFind)
+            {
+                Console.WriteLine("found: " + numberToFind);
+                return 0;
+            }
+
+            Console.WriteLine("not found: " + numberToFind);
+            return -1;
+        }
+
+        return ChopSlice(numberToFind, array_of_int, 0, length);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace take_1
 {
@@ -10,9 +11,11 @@ namespace take_1
 
             IChopper chopper = new BinaryChop();
 
+            Console.WriteLine(">>>>{ 1 }");
             assert_equal(-1, chopper.Chop(3, new int[] { }));
             assert_equal(-1, chopper.Chop(3, new int[] { 1 }));
             assert_equal(0, chopper.Chop(1, new[] { 1 }));
+            Console.WriteLine(">>>>{ 1, 3, 5 }");
             assert_equal(0, chopper.Chop(1, new int[] { 1, 3, 5 }));
             assert_equal(1, chopper.Chop(3, new int[] { 1, 3, 5 }));
             assert_equal(2, chopper.Chop(5, new int[] { 1, 3, 5 }));
@@ -20,6 +23,7 @@ namespace take_1
             assert_equal(-1, chopper.Chop(2, new int[] { 1, 3, 5 }));
             assert_equal(-1, chopper.Chop(4, new int[] { 1, 3, 5 }));
             assert_equal(-1, chopper.Chop(6, new int[] { 1, 3, 5 }));
+            Console.WriteLine(">>>>{ 1, 3, 5, 7 }");
             assert_equal(0, chopper.Chop(1, new int[] { 1, 3, 5, 7 }));
             assert_equal(1, chopper.Chop(3, new int[] { 1, 3, 5, 7 }));
             assert_equal(2, chopper.Chop(5, new int[] { 1, 3, 5, 7 }));
@@ -30,12 +34,28 @@ namespace take_1
             assert_equal(-1, chopper.Chop(6, new int[] { 1, 3, 5, 7 }));
             assert_equal(-1, chopper.Chop(8, new int[] { 1, 3, 5, 7 }));
 
-        }
+            var loong = new List<int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                loong.Add(i);
+            }
 
+            assert_equal(734, chopper.Chop(734, loong.ToArray()));
+            assert_equal(-1, chopper.Chop(1001, loong.ToArray()));
+
+            loong.Remove(734);
+            loong.ToArray();
+            assert_equal(-1, chopper.Chop(1001, loong.ToArray()));
+
+
+        }
 
         public static void assert_equal(int expected, int actual)
         {
-            Console.WriteLine($"{expected}:{actual} >> {expected == actual}");
+            if (expected != actual)
+            {
+                Console.WriteLine($"Expected index: {expected} returned: {actual} >> FAILED");
+            }
         }
     }
 
@@ -44,12 +64,5 @@ namespace take_1
         int Chop(int numberToFind, int[] array_of_int);
     }
 
-    public class BinaryChop : IChopper
-    {
-        public int Chop(int numberToFind, int[] array_of_int)
-        {
 
-            return -1;
-        }
-    }
 }
